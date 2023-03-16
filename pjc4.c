a4c2c075cbb40de3816e85c49e45121ec38fdef3
Pravin junne <pravinjunne@gmail.com>
2023-03-17 00:01:13 +0530

    find greater number

---
+++ pjc4.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{           //if else program to finde greater number
+    int a,b;
+    printf("Enter the A:");
+    scanf("%d",&a);
+    printf("Enter the B:");
+    scanf("%d",&b);
+    if(a>b)
+    {
+        printf("A is greater than B..");
+    }
+    else{
+        printf("B is greater than A");
+    }
+return 0;
+}
