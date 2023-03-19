1c7e95145078cc828adb8becc4aa9f16b4099e2e
Pravin junne <pravinjunne@gmail.com>
2023-03-19 23:43:13 +0530

    switch day selection

---
+++ pjc10.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main()
+{           //switch program for day selection.
+    int day;
+    printf("Enter any day number(1-7):");
+    scanf("%d",&day);
+    switch (day)
+    {
+    case 1: printf("Monday");
+        break;
+    case 2: printf("Tuesday");
+        break;
+    case 3: printf("Wednesday");
+        break;
+    case 4: printf("Thursday");
+        break;
+    case 5: printf("Friday");
+        break;
+    case 6: printf("Saturday");
+        break;
+    case 7: printf("Sunday");
+        break;
+    
+    default: printf("Wrong choice.");
+        break;
+    }
+return 0;
+}
