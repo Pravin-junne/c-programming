c205513ca68d2621311ae9595096679a25c1d54f
Pravin junne <pravinjunne@gmail.com>
2023-03-31 10:15:28 +0530

    while loop program to print numbers

---
+++ pjc12.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{       //while loop program to print numbers as per input.
+    int i=0,n;
+    printf("Enter the number:-");
+    scanf("%d",&n);
+    while(i<=n)
+    {
+        printf("%d\n",i);
+        i++;
+    }
+return 0;
+}
